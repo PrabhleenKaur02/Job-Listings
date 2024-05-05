10b8a58eaff157e3a84d81d39a699e521f57d7dd
PrabhleenKaur02 <prabhleen02info@gmail.com>
2024-05-05 15:14:11 +0530

    update remote

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import PageNotFound from "./pages/PageNotFound";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+  <Route index element={<HomePage />} />
+  <Route path="/jobs" element={<JobsPage />} />
+  <Route path='*' element={<PageNotFound />} />
+  </Route>
+  )
+)
+
+const App = () => {
+  return <RouterProvider router={router} />
+};
+
+
+export default App;
