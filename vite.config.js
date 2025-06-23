2bbf8e8666d9a8446497d296219b7734ddc5e453
PrabhleenKaur02 <prabhleen02info@gmail.com>
2025-06-23 22:50:06 +0530

    env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default({mode})=> {
+  const env = loadEnv(mode, process.cwd())
+
+ return defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': env.VITE_API_URL
       //  {
       //   // target: 'http://localhost:5000',
       //   changeOrigin: true,
@@ -16,4 +20,4 @@ export default defineConfig({
       // }
     }
   }
-})
+})}
